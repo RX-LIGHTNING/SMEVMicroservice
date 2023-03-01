bb5e38397b911fb424d901a53a67c7e0985716a4
Roman <mmamano@inbox.ru>
2023-03-01 16:43:17 +0300

    fixed merge conflict

--- src/main/java/com/example/smev/controller/SmevController.java
+++ src/main/java/com/example/smev/controller/SmevController.java
@@ -7,10 +7,7 @@ import com.example.smev.service.ResponseQueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -27,15 +24,13 @@ public class SmevController {
     public ResponseEntity<HttpStatus> requestFine(@RequestBody FineRequest fineRequest) {
         return new ResponseEntity<>(fineRequireService.saveFineRequestToQueue(fineRequest),HttpStatus.OK);
     }
-
-    // TODO: 21.02.23 Почему не GET маппинг. Мы тут только получаем ресурс
-    @PostMapping("/result")
-    public ResponseEntity<List<FineResponse>> getResult(@RequestBody UUID uuid) {
+    @GetMapping("/result/{uuid}")
+    public ResponseEntity<List<FineResponse>> getResult(@PathVariable UUID uuid) {
         return new ResponseEntity<>(fineResponseService.getFineResponse(uuid), HttpStatus.OK);
     }
 
-    @PostMapping("/acknowledge")
-    public ResponseEntity<HttpStatus> acknowledge(@RequestBody UUID uuid) {
+    @PostMapping("/acknowledge/{uuid}")
+    public ResponseEntity<HttpStatus> acknowledge(@PathVariable UUID uuid) {
         return new ResponseEntity<>(fineResponseService.deleteResponseFromQueue(uuid),HttpStatus.OK);
     }
 }
