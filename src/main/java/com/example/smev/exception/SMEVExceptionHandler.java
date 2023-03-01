4ff3b6ac3bf0af08f196a15bcb5414c6d1f53a2d
Roman <mmamano@inbox.ru>
2023-03-02 00:56:17 +0300

    added entity not found exception

--- src/main/java/com/example/smev/exception/SMEVExceptionHandler.java
+++ src/main/java/com/example/smev/exception/SMEVExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.smev.exception;
 
+import com.example.smev.dto.FineRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,5 +13,10 @@ public class SMEVExceptionHandler {
     protected ResponseEntity<Object> workerException() {
         log.error("Worker Exception");
         return ResponseEntity.badRequest().body("Something happened in worker");
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> entityNotFoundException(FineRequest fineRequest) {
+        log.error("Entity not Found uuid: {}", fineRequest.getUuid());
+        return ResponseEntity.badRequest().body("Entity not found");
     }
 }
